aaf427d13bcdf926547b9523b573af017f03fbd7
bnelad <106449445+bnelad@users.noreply.github.com>
2022-08-13 18:39:18 +0300

    Add files via upload

--- js/meme.controller.js
+++ js/meme.controller.js
@@ -15,27 +15,93 @@ function renderMeme() {
     const memeId = meme.selectedImgId
     const img = new Image()
     img.src = `images/${memeId}.jpg`
+    
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
-        console.log(meme.lines[0])
-        drawText(meme.lines[0].txt, 250,  50)    
+        drawText()     
     }
 }
 
-function drawText(txt, x, y) {
-    gCtx.beginPath()
-    gCtx.textBaseline = 'middle'
-    gCtx.textAlign = 'center'
-    gCtx.lineWidth = 1
-    gCtx.font = '40px david'
-    gCtx.fillStyle = 'yellow'
-    gCtx.fillText(txt, x, y)
-    gCtx.strokeStyle = 'green'
-    gCtx.strokeText(txt, x, y)
-    gCtx.closePath()
+function drawText() {
+    const lines = getMeme().lines
+    lines.forEach((line, idx) => {
+        gCtx.beginPath()
+        gCtx.textBaseline = 'middle'
+        gCtx.textAlign = 'center'
+        gCtx.lineWidth = 1
+        const fontSize = gMeme.lines[idx].size
+        gCtx.font = `${fontSize}px david`
+        const colorTxt = gMeme.lines[idx].color
+        gCtx.fillStyle = colorTxt
+        if (gMeme.selectedLineIdx === idx)
+            gCtx.strokeStyle = 'red'
+        else
+            gCtx.strokeStyle = 'black'
+        switch (gMeme.lines[idx].align) {
+            case 'left':
+                gCtx.fillText(line.txt, 100, line.y)
+                gCtx.strokeText(line.txt, 100, line.y)
+                break
+            case 'center':
+                gCtx.fillText(line.txt, 250, line.y)
+                gCtx.strokeText(line.txt, 250, line.y)
+                break
+            case 'right':
+                gCtx.fillText(line.txt, 450, line.y)
+                gCtx.strokeText(line.txt, 450, line.y)
+                break
+            default:
+        }
+        gCtx.closePath()
+    })
 }
 
 function onSetText(txt){
     setLineTxt(txt)
+    renderMeme()
+}
+
+function drawText2(txt, x, y) {
+    const lines = getMeme().lines
+    lines.forEach((line, idx) => {
+        gCtx.beginPath()
+        gCtx.textBaseline = 'middle'
+        gCtx.textAlign = 'center'
+        gCtx.lineWidth = 1
+        gCtx.font = '40px david'
+        gCtx.fillStyle = 'yellow'
+        gCtx.fillText(txt, x, y)
+        gCtx.strokeStyle = 'green'
+        gCtx.strokeText(txt, x, y)
+        gCtx.closePath()
+    })
+}
+
+function onNextLine() {
+    setNextLine()
+}
+
+function onSwitchLine() {
+    setSwitchLine()
+    renderMeme()
+}
+
+function onChangeFont(sizeChange) {
+    changeFont(sizeChange)
+    renderMeme()
+}
+
+function onChangeAlignment(newAlignment) {
+    changeAlignment(newAlignment)
+    renderMeme()
+}
+
+function onChangeColor(newColor) {
+    changeColor(newColor)
+    renderMeme()
+}
+
+function onChangeLocation(locationChange) {
+    changeLocation(locationChange)
     renderMeme()
 }
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -3,28 +3,16 @@
 var gKeywordSearchCountMap = {'funny': 12,'cat': 16, 'baby': 2}
 
 var gMeme = {
-    selectedImgId: 5,
-    selectedLineIdx: 0,
-    lines: [
-        {   
-        txt: 'I sometimes eat Falafel',
-        size: 20,
-        align: 'left',
-        color: 'red'
-        }
-    ]
-}
-
-var gMeme = {
     selectedImgId: 2,
     selectedLineIdx: 0,
     lines: [
         {   
-        txt: 'I sometimes eat Falafel',
-        size: 20,
-        align: 'left',
-        color: 'red'
-        }
+        txt: 'Edit text',
+        size: 35,
+        align: 'center',
+        color: 'white',
+        y: 50
+        } 
     ]
 }
 
@@ -34,9 +22,54 @@ function getMeme() {
 
 function setImg(imgId) {
     gMeme.selectedImgId = imgId
-
 }
 
 function setLineTxt(txt) {
     gMeme.lines[gMeme.selectedLineIdx].txt = txt
+}
+
+function setNextLine() {
+    gMeme.selectedLineIdx++
+    if (gMeme.selectedLineIdx === 1) {
+        const newLine = {   
+            txt: '',
+            size: 35,
+            align: 'center',
+            color: 'white',
+            y: 500
+            }
+        gMeme.lines.push(newLine)
+    } else {
+        const newLine = {   
+            txt: '',
+            size: 35,
+            align: 'center',
+            color: 'white',
+            y: 300
+            }
+        gMeme.lines.push(newLine)
+    }
+}
+
+function setSwitchLine() {
+    if (gMeme.selectedLineIdx === gMeme.lines.length - 1)
+        gMeme.selectedLineIdx = 0
+    else 
+        gMeme.selectedLineIdx++
+}
+
+function changeFont(sizeChange) {
+    gMeme.lines[gMeme.selectedLineIdx].size += sizeChange
+}
+
+function changeAlignment(newAlignment) {
+    gMeme.lines[gMeme.selectedLineIdx].align = newAlignment
+}
+
+function changeColor(newColor) {
+    gMeme.lines[gMeme.selectedLineIdx].color = newColor
+}
+
+function changeLocation(locationChange) {
+    gMeme.lines[gMeme.selectedLineIdx].y += locationChange
 }
